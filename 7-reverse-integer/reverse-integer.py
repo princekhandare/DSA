660d1da91080c9fbeb2817d42eff9d29acc45377
Prince Khandare <princekhandare660@gmail.com>
2026-06-12 21:50:24 +0530

    Time: 36 ms (5.87%) | Memory: 12.2 MB (99.19%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def reverse(self, x):
+        negative = x < 0
+        x = abs(x)
+
+        rev = 0
+
+        while x > 0:
+            digit = x % 10
+            rev = rev * 10 + digit
+            x = x // 10
+
+        if negative:
+            rev = -rev
+
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+
+        return rev
