23ec7b4cb5ebe5107a569744f91360b6c02cae72
Prince Khandare <princekhandare660@gmail.com>
2026-07-09 20:58:13 +0530

    Time: 36 ms (54.18%) | Memory: 13.2 MB (61.60%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJump(self, nums):
+        maxreach=0
+        for i in range(len(nums)):
+            if i > maxreach:
+                return False
+
+            maxreach=max(maxreach,i+nums[i])
+
+
+        return True
+        
+        
