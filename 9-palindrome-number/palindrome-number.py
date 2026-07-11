df8cb6b6a49f1196bdb9f3ff0b3b1a66713ff886
Prince Khandare <princekhandare660@gmail.com>
2026-07-11 12:05:47 +0530

    Time: 12 ms (49.22%) | Memory: 12.3 MB (53.98%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        s=str(x)
+        left=0;
+        right=len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left+=1
+            right-=1
+
+        return True    
+        
